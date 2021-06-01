8f6769713501afe7c3c2d4169f962cd18993777f
Gattuso <jgatt97@gmail.com>
2021-06-01 15:27:47 -0400

    okay

---
+++ app.py
@@ -0,0 +1,194 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv("metrics.csv") 
+data = data.dropna()
+data["Date"] = pd.to_datetime(data.time_started)
+#data = data.sort_values("Date", inplace=True)
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Dataset Refresh Analytics: Understand Your Datasets!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="NYSITS", className="header-emoji"),
+                html.H1(
+                    children="Dataset Refresh Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the behavior of the Python Refresh Process"
+                    " and the number of rows effected in a dataset",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region} for region in data.datasetName
+                            ],
+                            value="NYCC_BOATSFORHIRE",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                            ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                
+            ],
+            className="wrapper",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="update-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                
+            ],
+            className="wrapper",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="speed-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    [Output("price-chart", "figure"), Output("update-chart", "figure"), Output("speed-chart", "figure"),],
+    [
+        Input("region-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_charts(region, start_date, end_date):
+    mask = (
+        (data.datasetName == region)
+        & (data.Date >= start_date)
+        & (data.Date <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Created"],
+                "type": "lines",
+                "hovertemplate": "%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Rows Created",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+    UpdatedFigure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Updated"],
+                "type": "lines",
+                "hovertemplate": "%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Rows Updated",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+    speed = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": pd.to_datetime(filtered_data["time_finished"]) - pd.to_datetime(filtered_data["time_started"]),
+                "type": "lines",
+                "hovertemplate": "%{y}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Time for Update",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    return price_chart_figure, UpdatedFigure, speed
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
